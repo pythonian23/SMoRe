b5fe301f02400fc5240069a40b5dd3e6cf1f26df
pythonaian23 <pythonaian23@gmail.com>
2022-05-15 15:30:26 +0900

    detect and split tokens

--- render.go
+++ render.go
@@ -1,12 +1,32 @@
 package smore
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func tokenSplit(token string, parts []string) []string {
-	return parts
+	var out = make([]string, 0, cap(parts))
+	for _, part := range parts {
+		split := strings.Split(part, token)
+		for i, s := range split {
+			if i != 0 && i != len(split)-1 {
+				out = append(out, s, token)
+			} else {
+				out = append(out, s)
+			}
+		}
+	}
+	return out
 }
 
 func Render(md string) string {
 	var parts = []string{md}
+	parts = tokenSplit("```", parts)
+	parts = tokenSplit("**", parts)
+	parts = tokenSplit("__", parts)
+	parts = tokenSplit("*", parts)
+	parts = tokenSplit("_", parts)
+	parts = tokenSplit("`", parts)
 	return fmt.Sprint(parts)
 }
