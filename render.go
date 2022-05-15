bedda49b92d3f368e48f41f25ff82086ee43f414
pythonaian23 <pythonaian23@gmail.com>
2022-05-15 16:37:24 +0900

    cases

--- render.go
+++ render.go
@@ -1,7 +1,6 @@
 package smore
 
 import (
-	"fmt"
 	"strings"
 )
 
@@ -22,11 +21,78 @@ func tokenSplit(token string, parts []string) []string {
 
 func Render(md string) string {
 	var parts = []string{md}
-	parts = tokenSplit("```", parts)
-	parts = tokenSplit("**", parts)
-	parts = tokenSplit("__", parts)
-	parts = tokenSplit("*", parts)
-	parts = tokenSplit("_", parts)
-	parts = tokenSplit("`", parts)
-	return fmt.Sprint(parts)
+	parts = tokenSplit("```", parts) // codeBlock
+	parts = tokenSplit("\n\n", parts)
+	parts = tokenSplit("**", parts) // boldA
+	parts = tokenSplit("__", parts) // underline
+	parts = tokenSplit("\n", parts)
+	parts = tokenSplit("*", parts)  // italicA
+	parts = tokenSplit("_", parts)  // italicU
+	parts = tokenSplit("`", parts)  // code
+	parts = tokenSplit("\\", parts) // escaped
+
+	var out string
+	var (
+		codeBlock, boldA, underline, italicA, italicU, code, escaped bool
+	)
+	for _, part := range parts {
+		if escaped || (codeBlock && part != "```") || (code && part != "`") {
+			out += part
+			continue
+		}
+		switch part {
+		case "```":
+			if codeBlock {
+				// codeBlock terminate
+			} else {
+				// codeBlock start
+			}
+			codeBlock = !codeBlock
+		case "**":
+			if boldA {
+				// boldA terminate
+			} else {
+				// boldA start
+			}
+			boldA = !boldA
+		case "__":
+			if underline {
+				// underline terminate
+			} else {
+				// underline start
+			}
+			underline = !underline
+		case "*":
+			if italicA {
+				// italicA terminate
+			} else {
+				// italicA start
+			}
+			italicA = !italicA
+		case "_":
+			if italicU {
+				// italicU terminate
+			} else {
+				// italicU start
+			}
+			italicU = !italicU
+		case "`":
+			if code {
+				// code terminate
+			} else {
+				// code start
+			}
+			code = !code
+		case "\\":
+			if escaped {
+				// escaped terminate
+			} else {
+				// escaped start
+			}
+			escaped = !escaped
+		default:
+			out += part
+		}
+	}
+	return out
 }
